f286820ea0dd914e4642d5e7563e8502d84053ff
jerinmt <jerinmt379@gmail.com>
2025-11-19 21:43:18 +0530

    Completed day 6 homework

--- day6lab/classwork.sql
+++ day6lab/classwork.sql
@@ -1,3 +1,8 @@
+-- Add 3 books into the books table
+INSERT INTO books (title, author, genre, price, copiessold) VALUES ('The Silent Patient', 'Alex Michaelides', 'Thriller', 399.00, 1200), ('Atomic Habits', 'James Clear', 'Self-help', 499.00, 2000), ('The Psychology of Money', 'Morgan Housel', 'Finance', 350.00, 1800);
+-- Add 2 books into the bestsellers table
+INSERT INTO bestsellers (title, author, genre, price, copiessold) VALUES ('Ikigai', 'Francesc Miralles', 'Philosophy', 300.00, 2500),
+('Think Like a Monk', 'Jay Shetty', 'Self-help', 450.00, 2200);
 -- Show the list of all books from both tables, displaying only the title and author.
 SELECT title, author FROM books UNION SELECT title, author FROM bestsellers;
 -- Display all books from the books table where the price is greater than 400.
